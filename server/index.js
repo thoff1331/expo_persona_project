d3262047fbb61f341f03122ee9276c6683ebcdd9
Anand Tsogtbaatar <atsogt17@MacBook-Pro-2.local>
2019-05-14 01:50:01 -0500

    aws s3 setup

--- server/index.js
+++ server/index.js
@@ -3,6 +3,22 @@ const express = require("express");
 const app = express();
 const massive = require("massive");
 const session = require("express-session");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const fileType = require("file-type");
+const bluebird = require("bluebird");
+const multiparty = require("multiparty");
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+// configure AWS to work with promises
+AWS.config.setPromisesDependency(bluebird);
+
+// create S3 instance
+const s3 = new AWS.S3();
 
 app.use(express.json());
 const {
@@ -66,6 +82,35 @@ app.get("/api/portfolio", displayWork);
 app.delete("/api/portfolio/:id", deleteWork);
 app.put("/api/portfolio", editPortfolio);
 app.get("/auth/addLike/:id", addLikes);
+
+const uploadFile = (buffer, name, type) => {
+  const params = {
+    ACL: "public-read",
+    Body: buffer,
+    Bucket: process.env.S3_BUCKET,
+    ContentType: type.mime,
+    Key: `${name}.${type.ext}`
+  };
+  return s3.upload(params).promise();
+};
+app.post("/auth/picture", (request, response) => {
+  console.log("hitt");
+  const form = new multiparty.Form();
+  form.parse(request, async (error, fields, files) => {
+    if (error) throw new Error(error);
+    try {
+      const path = files.file[0].path;
+      const buffer = fs.readFileSync(path);
+      const type = fileType(buffer);
+      const timestamp = Date.now().toString();
+      const fileName = `bucketFolder/${timestamp}-lg`;
+      const data = await uploadFile(buffer, fileName, type);
+      return response.status(200).send(data);
+    } catch (error) {
+      return response.status(400).send(error);
+    }
+  });
+});
 
 app.listen(SERVER_PORT, () => {
   console.log(`Listening on port ${SERVER_PORT}`);
--- src/components/Portfolio/workAdd.js
+++ src/components/Portfolio/workAdd.js
@@ -19,8 +19,10 @@ export class workAdd extends Component {
       description: "",
       displayWork: [],
       showInput: true,
-      editForm: true
+      editForm: true,
+      file: null
     };
+    this.handleFileUpload = this.handleFileUpload.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -52,7 +54,40 @@ export class workAdd extends Component {
       this.setState({ showInput: false });
     }
   }
+  handleFileUpload(e) {
+    this.setState({ file: e.target.files });
+    console.log(e.target);
+  }
+  submitFile = event => {
+    event.preventDefault();
+    console.log("hitt");
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("file", this.state.file[0]);
+    axios
+      .post("/auth/picture", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      })
+      .then(response => {
+        console.log("hittem");
+        this.setState(
+          {
+            img: response.data.Location,
+            file: null
+          },
+          () => {
+            this.handleSubmit();
+          }
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
   render() {
+    // console.log(response.data.Location);
     return (
       <div>
         <Nav2 />
@@ -62,9 +97,10 @@ export class workAdd extends Component {
             <label>Image</label>
             <input
               placeHolder="Image"
-              onChange={this.handleChange}
-              value={this.state.name}
+              onChange={this.handleFileUpload}
+              // value={this.state.name}
               name="img"
+              type="file"
             />
             <label>Title</label>
             <input
@@ -94,9 +130,7 @@ export class workAdd extends Component {
               value={this.state.description}
               name="description"
             />
-            <button onClick={this.handleClick} onClick={this.handleSubmit}>
-              POST
-            </button>
+            <button onClick={this.submitFile}>POST</button>
           </div>
         </div>
       </div>
